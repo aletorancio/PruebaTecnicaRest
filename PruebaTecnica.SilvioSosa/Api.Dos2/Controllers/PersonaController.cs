48c04c0876dbb3c1c45f88bfd68d6330ec23b0d7
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2022-12-15 01:15:59 -0300

    Subido

---
+++ PruebaTecnica.SilvioSosa/Api.Dos2/Controllers/PersonaController.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data.SqlClient;
+
+namespace Api.Dos2.Controllers
+{
+    [ApiController]
+    [Route("Persona2")]
+    public class PersonaController : Controller
+    {
+        [HttpGet("Personas")]
+        public async Task<ActionResult<List<Persona>>> PersonasLista()
+        {
+            var respuesta = new List<Persona>();
+            var urlApiExterna = "https://localhost:7244/api/Persona";
+            using var client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetAsync(urlApiExterna);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return BadRequest(respuesta);
+                }
+
+                string jsonSerializado = await response.Content.ReadAsStringAsync();   
+                respuesta = JsonConvert.DeserializeObject<List<Persona>>(jsonSerializado)!;
+                //respuesta.Add(JsonConvert.DeserializeObject<Persona>(jsonSerializado)!);
+            }
+            catch(Exception e)
+            {
+                
+            }
+            return Ok(respuesta);
+        }
+
+        private readonly IConfiguration _config;
+        public PersonaController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("PersonaId")]
+        public async Task<ActionResult<Persona>> GetPersona(int personaId)
+        {
+            using var conexion = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var persona = await conexion.QueryFirstAsync<Persona>("SELECT * FROM Persona WHERE id = @Id",
+                new { Id = personaId });
+            return Ok(persona);
+        }
+    }
+}
