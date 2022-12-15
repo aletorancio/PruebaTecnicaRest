5d8f2561114c50f2e36a1f9c6521d2e15289fec1
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2022-12-15 10:59:42 -0300

    Delete PruebaTecnica.SilvioSosa directory

--- PruebaTecnica.SilvioSosa/Api.Dos/Persona.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api.Dos2
-{
-    public class Persona
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string Apellido { get; set; } = string.Empty;
-    }
-}
--- PruebaTecnica.SilvioSosa/Api.Uno/Persona.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api.Uno
-{
-    public class Persona
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string Apellido { get; set; } = string.Empty;
-    }
-}
